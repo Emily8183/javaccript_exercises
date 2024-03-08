227541db1d4d305129082ca1595c782a40004811
Emily <emily.zy.lin@gmail.com>
2024-03-08 14:28:10 -0600

    added itr.next() to go through the element in the iterator object one by one

--- Iterables_and_iterators.js
+++ Iterables_and_iterators.js
@@ -3,3 +3,12 @@ let arr1 = [1, 2, 3, 4];
 for (let i of arr1) {
   console.log(i);
 }
+
+// the iterator function below to create an iterator object for the array arr2.
+let arr2 = [1, 2, 3, 4];
+let itr = arr2[Symbol.iterator]();
+//The printint result will be: Object [Array Iterator] {}
+//console.log(itr);
+
+//itr.next() will iterate through every element one by one
+console.log(itr.next());
