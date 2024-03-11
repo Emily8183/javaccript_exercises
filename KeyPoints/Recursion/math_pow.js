9399616c360b030b8ad9df89caa70f379b326cb3
Emily <emily.zy.lin@gmail.com>
2024-03-10 23:08:26 -0500

    updated math pow function

--- KeyPoints/Recursion/math_pow.js
+++ KeyPoints/Recursion/math_pow.js
@@ -3,6 +3,8 @@
 // power(2,4) // 16
 
 function power(baseNum, exponent) {
-  if (num === 0) return 1;
-  return num * power(num - 1);
+  if (exponent === 0) return 1;
+  return baseNum * power(baseNum, exponent - 1);
 }
+
+console.log(power(2, 4));
