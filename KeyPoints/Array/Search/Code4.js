548f33c7a76d2990a43f1c7a8876195fcd5094cd
Emily <emily.zy.lin@gmail.com>
2024-03-14 14:41:57 -0500

    updated the results of Array search

--- KeyPoints/Array/Search/Code4.js
+++ KeyPoints/Array/Search/Code4.js
@@ -1,5 +1,9 @@
-let arr=[2300,4500,5600,7800,1200];
-const rVal = arr.find((elem)=>{
-   return elem>3000;
-})
+let arr = [2300, 4500, 5600, 7800, 1200];
+const rVal = arr.find((elem) => {
+  //only return the first element that satisfies the condition
+  return elem > 3000;
+});
 console.log(rVal);
+/*
+  Output: 4500
+*/
--- KeyPoints/Array/Search/Code7.js
+++ KeyPoints/Array/Search/Code7.js
@@ -1,5 +1,3 @@
-let arr = [2300, 4500, 5600, 7800, 1200];
-const rVal = arr.filter((elem) => {
-  return elem > 3000;
-});
-console.log(rVal);
+let arr = ["One", "Two", "Three", "Four", "Five"];
+console.log(arr.lastIndexOf("Abcd"));
+// Output: -1
