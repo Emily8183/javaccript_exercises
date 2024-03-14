10503408e3762ab7659c123f511af0a8cbf01db7
Emily <emily.zy.lin@gmail.com>
2024-03-14 14:55:53 -0500

    updated the notes in Array Add_Remove

--- KeyPoints/Array/Add_Remove/Code11.js
+++ KeyPoints/Array/Add_Remove/Code11.js
@@ -1,3 +1,3 @@
-let arr = ["One","Two","Three","Four","Five"];
-console.log(arr.splice(0,0,"New"));
-console.log(arr);
+let arr = ["One", "Two", "Three", "Four", "Five"];
+console.log(arr.splice(0, 0, "New")); // indicating that no elements were removed from the array.
+console.log(arr); //[ 'New', 'One', 'Two', 'Three', 'Four', 'Five' ]
--- KeyPoints/Array/Add_Remove/Code8.js
+++ KeyPoints/Array/Add_Remove/Code8.js
@@ -1,3 +1,3 @@
-let arr = ["One","Two","Three","Four","Five"];
-console.log(arr.splice(2,1,"New","Another"));
+let arr = ["One", "Two", "Three", "Four", "Five"];
+console.log(arr.splice(2, 1, "New", "Another")); //"Three"
 console.log(arr);
