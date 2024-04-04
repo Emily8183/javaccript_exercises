cefa3a51255db987f7880f47bdb8b1bb3a9ff45a
Emily <emily.zy.lin@gmail.com>
2024-04-04 17:37:03 -0500

    added three drums' sounds

--- KeyPoints/AngelaYuUdemyQuiz/134_Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ KeyPoints/AngelaYuUdemyQuiz/134_Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,1 +1,32 @@
-//create a function that
+//a high order function that play a drum by clicking on it
+
+//creates a function that add an eventhandler to each drum button
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    switch (this.innerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/" + "tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "a":
+        var tom2 = new Audio("sounds/" + "tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "s":
+        var tom3 = new Audio("sounds/" + "tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "d":
+        var tom4 = new Audio("sounds/" + "tom-4.mp3");
+        tom4.play();
+        break;
+
+      default:
+        console.log(this.innerHTML);
+    }
+  });
+}
