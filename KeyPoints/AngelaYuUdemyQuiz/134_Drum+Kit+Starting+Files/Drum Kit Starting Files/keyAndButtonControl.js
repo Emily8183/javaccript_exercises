7074f7671f5d2ae1cabdbd5cb58bb4d0d6811046
Emily <emily.zy.lin@gmail.com>
2024-04-04 20:07:32 -0500

    added button and key mixed version

---
+++ KeyPoints/AngelaYuUdemyQuiz/134_Drum+Kit+Starting+Files/Drum Kit Starting Files/keyAndButtonControl.js
@@ -0,0 +1,27 @@
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+  }
+}
