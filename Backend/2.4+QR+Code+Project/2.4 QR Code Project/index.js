d946e86178d91f2aebc802c27320139e380e644a
Emily <emily.zy.lin@gmail.com>
2024-04-05 23:42:29 -0500

    finished the QRcode generator

---
+++ Backend/2.4+QR+Code+Project/2.4 QR Code Project/index.js
@@ -0,0 +1,39 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+import qr from "qr-image";
+import inquirer from "inquirer";
+import fs from "node:fs";
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      message: "what URL do you have?",
+      name: "url",
+      //The name to use when storing the answer in the answers hash.
+    },
+  ])
+  .then((answers) => {
+    console.log(answers.url);
+    var qr_svg = qr.image(answers.url, { type: "png" });
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt", answers.url, (err) => {
+      if (err) {
+        console.error("Error writing URL to file:", err);
+      } else {
+        console.log("URL saved to URL.txt");
+      }
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.error("failed");
+    } else {
+      console.error("Error:", error);
+    }
+  });
