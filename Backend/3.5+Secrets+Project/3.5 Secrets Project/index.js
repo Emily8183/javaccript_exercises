ea094e3ab4802d97f28ee1ad1e78e76e4d1cdd4e
Emily <emily.zy.lin@gmail.com>
2024-04-10 22:58:46 -0500

    finished the session 206

--- Backend/3.5+Secrets+Project/3.5 Secrets Project/index.js
+++ Backend/3.5+Secrets+Project/3.5 Secrets Project/index.js
@@ -6,13 +6,33 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+let userPassword = "";
+//can set up a boolean to authorize the password: var userIsAuthorized = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkPassword(req, res, next) {
+  userPassword = req.body["password"];
+  next();
+}
+
+app.use(checkPassword);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userPassword === "ILoveProgramming") {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
 });
 
 app.listen(port, () => {
