55a5c056ae28021a1046f36bda081ebf79f79aa3
Emily <emily.zy.lin@gmail.com>
2024-04-11 16:50:04 -0500

    finished session 208

--- Backend/4.0+EJS/4.0 EJS/index.js
+++ Backend/4.0+EJS/4.0 EJS/index.js
@@ -1,23 +1,25 @@
 import express from "express";
-import ejs from "ejs";
 
 const app = express();
-
 const port = 3000;
 
 app.set("view engine", "ejs");
 
-// function weatherAdvice (req, res, next) {
-//     res.render("index.ejs",
-//     {dayType: req.body["dayType"],
-//     advice:req.body["advice"] })
-//     next();
-//   }
-
-// app.use(weatherAdvice);
-
 app.get("/", (req, res) => {
-  res.render("index");
+  let currentDay = new Date().getDay();
+
+  let type = "Weekend";
+  let adv = "Enjoy your weeekend!";
+
+  if (currentDay !== 0 || currentDay !== 6) {
+    type = "workday";
+    adv = "get to work!";
+  }
+
+  res.render("index", {
+    dayType: type,
+    advice: adv,
+  });
   //res.render()will look in a views folder for the view. So you only have to define pages/index since the full path is views/pages/index.
 });
 
