5de3d04ba0f0125b4e40ca7b3622669da0e64821
Emily <emily.zy.lin@gmail.com>
2024-04-11 14:35:49 -0500

    tested successfully for loading ejs homepage

--- Backend/4.0+EJS/4.0 EJS/index.js
+++ Backend/4.0+EJS/4.0 EJS/index.js
@@ -5,6 +5,22 @@ const app = express();
 
 const port = 3000;
 
+app.set("view engine", "ejs");
+
+// function weatherAdvice (req, res, next) {
+//     res.render("index.ejs",
+//     {dayType: req.body["dayType"],
+//     advice:req.body["advice"] })
+//     next();
+//   }
+
+// app.use(weatherAdvice);
+
+app.get("/", (req, res) => {
+  res.render("index");
+  //res.render()will look in a views folder for the view. So you only have to define pages/index since the full path is views/pages/index.
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
