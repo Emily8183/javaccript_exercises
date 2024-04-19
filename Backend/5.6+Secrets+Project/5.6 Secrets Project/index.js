6fb56cc80ddd8345ec1d0a03196912ef4526a52f
Emily <emily.zy.lin@gmail.com>
2024-04-19 09:20:34 -0500

    set up the basic res.render

--- Backend/5.6+Secrets+Project/5.6 Secrets Project/index.js
+++ Backend/5.6+Secrets+Project/5.6 Secrets Project/index.js
@@ -11,3 +11,23 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/random";
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    secret: "secret",
+    user: "user",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
