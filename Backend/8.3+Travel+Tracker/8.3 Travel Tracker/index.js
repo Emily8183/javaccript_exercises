f1ab6c99bbc0c230e055bd88ef629ca09b70cd34
Emily <emily.zy.lin@gmail.com>
2024-04-23 13:33:52 -0500

    set up db

--- Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -1,8 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "",
+  port: 5432,
+});
+
+db.connect();
+
+// db.query("SELECT * FROM visited_country", (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+
+//   }
+// }))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
