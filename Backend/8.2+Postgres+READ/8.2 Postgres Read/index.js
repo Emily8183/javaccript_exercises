aeb40406111320574c55ee4c971ec7c210758a33
Emily <emily.zy.lin@gmail.com>
2024-04-22 23:03:18 -0500

    set up db.query

--- Backend/8.2+Postgres+READ/8.2 Postgres Read/index.js
+++ Backend/8.2+Postgres+READ/8.2 Postgres Read/index.js
@@ -5,13 +5,28 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
-const pool = new pg.Pool({
+const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "flags",
   password: "",
   port: 5432,
 });
+
+db.connect();
+
+const quiz = [];
+
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+
+  }
+  db.end();
+});
+ 
 
 let totalCorrect = 0;
 
