1929425266add4c84e65212ea04acd3019a873e9
Emily <emily.zy.lin@gmail.com>
2024-04-23 21:55:02 -0500

    finish session 255

--- Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -15,15 +15,6 @@ const db = new pg.Client({
 
 db.connect();
 
-// db.query("SELECT * FROM visited_countries", (err, res) => {
-//   if (err) {
-//     console.log("Error executing query", err.stack);
-//   } else {
-//     visited_countries = res.rows;
-//     console.log(visited_countries);
-//   }
-// });
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -41,8 +32,35 @@ app.get("/", async (req, res) => {
   } catch (err) {
     console.log("Error executing query", err.stack);
     res.status(500).send("Internal Server Error");
-  } finally {
-    db.end();
+  }
+  // finally {
+  //   db.end();
+  // }
+  //Must remove db.end() otherwise the database will close
+});
+
+//INSERT INTO visited_countries (country_code) VALUES ()
+app.post("/add", async (req, res) => {
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1",
+      // select the country name from the countries table, find the country_code
+      // $1为占位符
+      [req.body["country"]]
+      // 代入req.body["country"],因为是array，所以外面要用[]
+    );
+
+    //After found the country name, insert the country_code into the visited_countries table
+    if (result.rows.length !== 0) {
+      const data = result.rows[0];
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [data.country_code]
+      );
+      res.redirect("/");
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
 
