1435b5d9b641c054aff610256f5866a4af24e04f
Emily <emily.zy.lin@gmail.com>
2024-04-25 11:31:03 -0500

    bind the db with index

--- Backend/Permalist+Project/8.6 Permalist Project/solution.js
+++ Backend/Permalist+Project/8.6 Permalist Project/solution.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "permalist",
-  password: "123456",
+  password: "W4vTqMRgcuiERpa",
   port: 5432,
 });
 db.connect();
