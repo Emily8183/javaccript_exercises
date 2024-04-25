51ed868c2bb8dddddfc195141124ba37014eb0a7
Emily <emily.zy.lin@gmail.com>
2024-04-25 14:53:03 -0500

    fix a bit duplicates of /get route but still need some other fix

--- Backend/Permalist+Project/8.6 Permalist Project/index.js
+++ Backend/Permalist+Project/8.6 Permalist Project/index.js
@@ -17,33 +17,67 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-async function showItemList() {
-  const result = await db.query("SELECT * FROM items");
-  let items = [];
-  result.rows.forEach((item) => {
-    items.push({
-      id: item.id,
-      title: item.title,
-    });
-  });
-  return items;
+let items1 = [];
+let items2 = [];
+// let lastFetchedItemId = 0;
+
+// Return the items since the last fetched item
+// async function fetchNewItems() {
+//   const result = await db.query(
+//     "SELECT * FROM items WHERE id > $1 ORDER BY id ASC",
+//     [lastFetchedItemId]
+//   );
+
+//   result.rows.forEach((item) => {
+//     items.push({
+//       id: item.id,
+//       title: item.title,
+//     });
+//   });
+// }
+
+async function showTheLastItem() {
+  const result = await db.query(
+    "SELECT * FROM items WHERE id=(SELECT MAX(id) FROM items)"
+  );
+
+  items1 = result.rows.map((item) => ({
+    id: item.id,
+    title: item.title,
+  }));
+
+  return items1;
+}
+
+async function showExistingList() {
+  const result = await db.query("SELECT * FROM items ORDER BY id DESC");
+  items2 = result.rows.map((item) => ({
+    id: item.id,
+    title: item.title,
+  }));
+  return items2;
 }
 
 app.get("/", async (req, res) => {
-  const items = await showItemList();
+  const item1 = await showTheLastItem();
+  const items2 = await showExistingList();
+
   res.render("index.ejs", {
     listTitle: "Today",
-    listItems: items,
+    listItems: [...items1, ...items2],
   });
 });
 
-// app.post("/add", (req, res) => {
-//   const item = req.body.newItem;
-//   items.push({ title: item });
-//   res.redirect("/");
-// });
+app.post("/add", async (req, res) => {
+  const newItem = req.body.newItem;
 
-// app.post("/edit", (req, res) => {});
+  await db.query("INSERT INTO items (title) VALUES ($1)", [newItem]);
+  // items.push({ title: item }); <= we don't need this since we want to save the data in SQL not into a local array
+
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
 
 // app.post("/delete", (req, res) => {});
 
