84552835f44509eeb1becb56a1adfd966ee77821
Emily <emily.zy.lin@gmail.com>
2024-04-25 22:29:16 -0500

    add notes

--- Backend/9.3+Sessions+and+Cookies/solution.js
+++ Backend/9.3+Sessions+and+Cookies/solution.js
@@ -15,8 +15,11 @@ env.config();
 app.use(
   session({
     secret: "TOPSECRETWORD",
+    //这个string可以是任意的，随便想一个
     resave: false,
+    //means if you want to save the session in postgres
     saveUninitialized: true,
+    //means if you want to store the uninitialized sessions into our server memory.
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,6 +27,7 @@ app.use(express.static("public"));
 
 app.use(passport.initialize());
 app.use(passport.session());
+//这两排必须在session设置的后面
 
 const db = new pg.Client({
   user: "postgres",
@@ -57,6 +61,7 @@ app.get("/logout", (req, res) => {
 
 app.get("/secrets", (req, res) => {
   // console.log(req.user);
+  //  req.isAuthenticated() is a function that this comes from Passport and it allows us to check, "Is the current user who's logged in in the current session authenticated already?"
   if (req.isAuthenticated()) {
     res.render("secrets.ejs");
   } else {
@@ -107,6 +112,8 @@ app.post("/register", async (req, res) => {
 
 passport.use(
   new Strategy(async function verify(username, password, cb) {
+    // it's a local strategy means it's trying to validate whether if a user already has the right password, if their email exists in the database already,
+    // "cb" means callback here
     try {
       const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
         username,
@@ -114,11 +121,13 @@ passport.use(
       if (result.rows.length > 0) {
         const user = result.rows[0];
         const storedHashedPassword = user.password;
+        // It can automatically through the use of this verify() function, grab hold of the username and password from the form that submits the login request.
         bcrypt.compare(password, storedHashedPassword, (err, valid) => {
           if (err) {
             //Error with password check
             console.error("Error comparing passwords:", err);
             return cb(err);
+            //this callback对应的是上面check req.isAuthenticated()的部分
           } else {
             if (valid) {
               //Passed password check
@@ -140,9 +149,12 @@ passport.use(
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
+  //serializeUser is a function that save the data of the user who's logged in to local storage, we can use this callback to pass over any of the details of the user
+  // // 将用户对象中的标识符（例如用户 ID）存储到会话中
 });
 passport.deserializeUser((user, cb) => {
   cb(null, user);
+  // deserializeUser 将会话中存储的用户标识符用于检索相应的用户对象，并将其传递给回调函数。
 });
 
 app.listen(port, () => {
