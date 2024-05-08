eace8dd0caa40939fe4b2a807875b256ed71ba3c
Emily <emily.zy.lin@gmail.com>
2024-05-08 13:52:08 -0500

    finish the destructing chapter

--- KeyPoints/destructuring/data.js
+++ KeyPoints/destructuring/data.js
@@ -24,9 +24,31 @@ console.log(girls);
 
 //give a customized value to a variable
 console.log("give a customized value to a variable");
-const { name = "Mary", favorite = "fruits" } = girls;
-console.log(name);
-console.log(favorite);
+// const { name = "Mary", favorite = "fruits" } = girls;
+// console.log(name);
+// console.log(favorite);
 //当没有name或者favorite value时，可作为default value
 
-//
+//desctruct nested object (extension destruct)
+console.log("desctruct nested object");
+const animals = [
+  {
+    name: "sheep",
+    favorite: "grass",
+    home: { place: "zoo", size: 25 },
+  },
+  {
+    name: "tiger",
+    favorite: "meat",
+  },
+];
+
+const [sheep, tiger] = animals;
+
+const {
+  name,
+  favorite,
+  home: { place, size },
+} = sheep;
+
+console.log(place);
