3e723c8f6e0ac499ed908381af33462e6a05c8bd
Emily <emily.zy.lin@gmail.com>
2024-05-08 13:02:06 -0500

    give a customized value to a variable

--- KeyPoints/destructuring/data.js
+++ KeyPoints/destructuring/data.js
@@ -1,15 +1,32 @@
 const human = [
-  { name: "girls", favorite: "flowers" },
-  { name: "boys", favourite: "cars" },
+  { name: "Mary", favorite: "flowers" },
+  { name: "boys", favorite: "cars" },
 ];
 
 //print out two objects
+console.log("print out two objects");
 console.log(human);
 
 //print out "girls" only
+console.log("print out 'girls' only");
 const [girls, boys] = human;
 console.log(girls);
 
 //destruct an object
-const { name, favorite } = girls;
+// const { name, favorite } = girls;
+// console.log(favorite);
+
+//prove an alternative name
+// console.log("prove an alternative name");
+// const { name: girlsName, favorite: girlsFavorite } = girls;
+// console.log(girlsName);
+// console.log(girlsFavorite);
+
+//give a customized value to a variable
+console.log("give a customized value to a variable");
+const { name = "Mary", favorite = "fruits" } = girls;
+console.log(name);
 console.log(favorite);
+//当没有name或者favorite value时，可作为default value
+
+//
